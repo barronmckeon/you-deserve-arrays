e9bd690886dd0c915d73c816b2c24fcf3605bb37
Barron Mckeon <barronmckeon@Barrons-MacBook-Pro.local>
2022-02-14 15:43:13 -0500

    rev1

---
+++ getFirstItemFrom.js
@@ -0,0 +1,51 @@
+
+
+const prompt = require('prompt-sync')({sigint: true});
+
+/*
+let userArray = JSON.parse(prompt("Please enter an array: " ));
+let userItem= userArray[0]
+
+console.log("The first item is: " + userItem);
+
+
+// ["Hello", "Darkness", "MyOnly", "Friend"]
+
+*/
+
+
+
+
+
+let userArray = JSON.parse(prompt("Please enter an array: " ));
+let userItem= userArray[userArray.length-1]
+
+console.log("The last item is: " + userItem  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*      if arrayLength >= 3 
+consolr.log
+else 
+*/
+
+
+/*
+for (let count = 0; count <= 10++;) {
+    console.log(count);
+}*/
